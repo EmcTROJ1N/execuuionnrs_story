f55f83d67ef59ac601648a9b2f85d6dc8d95ebd2
Julien Bisconti <julien.bisconti@gmail.com>
2020-08-17 19:11:16 +0300

    hot fix bugs

--- Executioners_story/Assets/Scripts/about.cs
+++ Executioners_story/Assets/Scripts/about.cs
@@ -7,23 +7,29 @@ public class about : MonoBehaviour
   public GameObject menu;
   public GameObject game;
   public GameObject gg;
-  public GameObject saydialog;
+  public GameObject saydialog_1;
+  public GameObject saydialog_2;
   public GameObject menu_vibor;
+  public GameObject stage;
   public void About()
   {
     if(PlayerPrefs.GetInt("completes") >= 1)
     {
         menu.SetActive(true);
         game.SetActive(false);
-        saydialog.SetActive(false);
+        saydialog_1.SetActive(false);
+        saydialog_2.SetActive(false);
         menu_vibor.SetActive(false);
+        stage.SetActive(false);
     }
     else
     {
         game.SetActive(false);
         gg.SetActive(true);
-        saydialog.SetActive(false);
+        saydialog_1.SetActive(false);
+        saydialog_2.SetActive(false);
         menu_vibor.SetActive(false);
+        stage.SetActive(false);
     }
   }
 }
